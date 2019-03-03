fe8492678bcf0cd20e2e0f893051c528e5be6248
Aljaz Kosirnik <aljaz.kosirnik1@gmail.com>
2019-03-03 21:10:37 +0100

    Verzija2

--- Movies/main.py
+++ Movies/main.py
@@ -12,7 +12,7 @@ from google.appengine.api import users
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=False)
 
-stran = 1
+
 request = Request.blank('/')
 
 
@@ -37,10 +37,20 @@ class BaseHandler(webapp2.RequestHandler):
 
 class MainHandler(BaseHandler):
     def get(self):
-        global stran
+        return self.render_template("index.html")
 
+    def post(self):
+        return self.redirect_to("filmi")
+
+
+class StranHandler(BaseHandler):
+    def get(self, stran):
+
+        # Get popular data from API based on page client is on
         vsi = []
         img_path = "https://image.tmdb.org/t/p/w500/"
+
+        # variable "stran" is from url example /stran/1/ , /stran/2/
         url = "https://api.themoviedb.org/3/movie/popular?api_key=26df55ce52689aa1d352444f3918da77&language=en-US&page=" + str(stran)
         response_url = urlopen(url)
         data = json.loads(response_url.read().decode("utf-8"))
@@ -53,62 +63,51 @@ class MainHandler(BaseHandler):
             "stran": stran
         }
 
-        return self.render_template("index.html", params=params)
+        return self.render_template("stran.html", params=params)
 
-    def post(self):
-        global stran
-        global film_id
-        domov_gumb = self.request.get("domov")
+    def post(self, stran):
+        # GET all buttons, and call the function if they are pressed
         prejsnji_gumb = self.request.get("prejsna-stran")
         naslednji_gumb = self.request.get("naslednja-stran")
         vec_gumb = self.request.get("vec")
         dodaj_gumb = self.request.get("dodaj")
+        nova_stran = int(stran)
 
-        if domov_gumb:
-            stran = 1
-        elif prejsnji_gumb:
-            if stran == 1:
-                stran = 1
+        # If previous or next button are pressed add or subtract the page number
+        if prejsnji_gumb:
+            if nova_stran == 1:
+                nova_stran = 1
+                stran = nova_stran
+                self.redirect("/stran/" + str(stran) + "/")
             else:
-                stran -= 1
+                nova_stran -= 1
+                stran = nova_stran
+                self.redirect("/stran/" + str(stran) + "/")
         elif naslednji_gumb:
-            stran += 1
+            nova_stran += 1
+            stran = nova_stran
+            self.redirect("/stran/" + str(stran) + "/")
 
         if vec_gumb:
             return self.redirect_to("film")
         elif dodaj_gumb:
-            film_id = self.request.get("film_id")
-            naziv = self.request.get("naziv")
-            opis = self.request.get("opis")
-            img = self.request.get("img")
-            ocena = self.request.get("ocena")
-            datum_izdaje = self.request.get("datum_izdaje")
-
-            filmi = PriljubljeniFilmi(film_id=film_id, naziv=naziv, opis=opis, img=img, ocena=ocena, datum_izdaje=datum_izdaje)
+            # Add this film_id to GAE to favorites
+            filmi = PriljubljeniFilmi(film_id=self.request.get("film_id"), naziv=self.request.get("naziv"), opis=self.request.get("opis"),
+                                      img=self.request.get("img"), ocena=self.request.get("ocena"), datum_izdaje=self.request.get("datum_izdaje"))
             filmi.put()
             return self.redirect_to("moji-filmi")
-        return self.redirect_to("filmi")
 
 
 class FilmHandler(BaseHandler):
     def get(self, film_id):
-
+        # Api is for specific movie that I get with film_id
         url = "https://api.themoviedb.org/3/movie/" + film_id + "?api_key=26df55ce52689aa1d352444f3918da77&language=en-US"
         response_url = urlopen(url)
         data = json.loads(response_url.read().decode("utf-8"))
 
-        id_s = data["id"]
         slika = "https://image.tmdb.org/t/p/w500/" + data["poster_path"]
         back_slika = "https://image.tmdb.org/t/p/original" + data["backdrop_path"]
-        naziv = data["title"]
-        ocena = data["vote_average"]
-        datum_izdaje = data["release_date"]
-        opis = data["overview"]
-        kratek_opis = data["tagline"]
-        proracun = data["budget"]
-        zasluzek = data["revenue"]
-        cas = data["runtime"]
-        # Spodaj so listi
+
         zvrsti = []
         produkcija = []
         produkcija_img = []
@@ -126,15 +125,15 @@ class FilmHandler(BaseHandler):
             produkcija_img.append(produkcija_list[p]["logo_path"])
             p += 1
 
-        # Video ne dela ker je prevec http requestov na enem page-u?   ---   raise HTTPError(req.get_full_url(), code, msg, hdrs, fp) HTTPError: HTTP Error 429: Unknown   ---
-        # video_url = "https://api.themoviedb.org/3/movie/" + film_id + "/videos?api_key=26df55ce52689aa1d352444f3918da77&language=en-US"
-        # video_response_url = urlopen(video_url)
-        # video_data = json.loads(video_response_url.read().decode("utf-8"))
-        # video = "https://www.youtube.com/embed/" + video_data["results"][0]["key"]
+        video_url = "https://api.themoviedb.org/3/movie/" + film_id + "/videos?api_key=26df55ce52689aa1d352444f3918da77&language=en-US"
+        video_response_url = urlopen(video_url)
+        video_data = json.loads(video_response_url.read().decode("utf-8"))
+        video = "https://www.youtube.com/embed/" + video_data["results"][0]["key"]
 
-        params = {"slika": slika, "back_slika": back_slika, "film_id": id_s, "naziv": naziv, "ocena": ocena, "datum_izdaje": datum_izdaje, "opis": opis, "kratek_opis": kratek_opis,
-                  "proracun": proracun, "zasluzek": zasluzek, "cas": cas, "zvrsti": zvrsti, "produkcija": produkcija, "produkcija_img": produkcija_img}
-        # , "video": video}
+        # Pass trough the page all that I get with the API
+        params = {"slika": slika, "back_slika": back_slika, "film_id": data["id"], "naziv": data["title"], "ocena": data["vote_average"], "datum_izdaje": data["release_date"],
+                  "opis": data["overview"], "kratek_opis": data["tagline"], "proracun": data["budget"], "zasluzek": data["revenue"], "cas": data["runtime"], "zvrsti": zvrsti,
+                  "produkcija": produkcija, "produkcija_img": produkcija_img, "video": video}
 
         return self.render_template("film.html", params=params)
 
@@ -150,18 +149,20 @@ class MojiFilmiHandler(BaseHandler):
 
 class IzbrisiHandler(BaseHandler):
     def get(self, film_id):
+        # Delete movie from favorites
         film = PriljubljeniFilmi.get_by_id(int(film_id))
         film.key.delete()
-        return self.redirect_to("filmi")
+        return self.redirect_to("moji-filmi")
 
     def post(self, film_id):
         film = PriljubljeniFilmi.get_by_id(int(film_id))
         film.key.delete()
-        return self.redirect_to("filmi")
+        return self.redirect_to("moji-filmi")
 
 
 app = webapp2.WSGIApplication([
-    webapp2.Route('/', MainHandler, name="filmi"),
+    webapp2.Route('/', MainHandler),
+    webapp2.Route('/stran/<stran:(\d+)>/', StranHandler, name="filmi"),
     webapp2.Route('/film/<film_id:(\d+)>/', FilmHandler, name="film"),
     webapp2.Route('/moji-filmi/', MojiFilmiHandler, name="moji-filmi"),
     webapp2.Route('/izbrisi/<film_id:\d+>/', IzbrisiHandler),
